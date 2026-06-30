146cb244a3ca0706a306d955855978c8b36701a9
Yassine Ben Fatah <94804124+kramatile@users.noreply.github.com>
2026-06-30 15:48:06 +0100

    Solve jaccard-similarity

---
+++ jaccard-similarity/jaccard-similarity.py
@@ -0,0 +1,12 @@
+def jaccard_similarity(set_a, set_b):
+    """
+    Compute the Jaccard similarity between two item sets.
+    """
+    # Write code here
+    if not set_a and not set_b :
+        return 0.0
+    set_a = set(set_a)
+    set_b = set(set_b)
+    intersection = set_a & set_b
+    union = set_a | set_b
+    return len(intersection) / len(union)
