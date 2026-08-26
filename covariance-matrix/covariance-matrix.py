a7f3fc0d11273a710560cc69dbff7feb54a6e147
Yassine Ben Fatah <94804124+kramatile@users.noreply.github.com>
2026-08-26 13:40:45 +0200

    Solve covariance-matrix

--- covariance-matrix/covariance-matrix.py
+++ covariance-matrix/covariance-matrix.py
@@ -4,10 +4,8 @@ def covariance_matrix(X: list) -> np.ndarray:
     """
     Returns the covariance matrix as a NumPy array.
     """
-    X=np.array([np.array(xi,dtype=float) for xi in X])
+    X=np.array(X,dtype=float)
     Mu = np.mean(X,axis=0)
-    for j in range(len(X[0])):
-        X[:,j] = X[:,j] - Mu[j]
-    Xt = np.transpose(X)
-    cov = (Xt @ X)/(len(X) - 1)
+    X = X - Mu
+    cov = (X.T @ X)/(len(X) - 1)
     return cov
