c69ae761c0bc0f71e07077f23d4a775bfc2ee6f5
Yassine Ben Fatah <94804124+kramatile@users.noreply.github.com>
2026-08-31 14:16:16 +0200

    Solve zscore-standardization

--- zscore-standardization/zscore-standardization.py
+++ zscore-standardization/zscore-standardization.py
@@ -4,4 +4,5 @@ def zscore_standardize(X: list, axis: int = 0, eps: float = 1e-12) -> np.ndarray
     X = np.asarray(X, dtype=float)
     mu = np.mean(X, axis=axis, keepdims=True)
     sigma = np.std(X, axis=axis, keepdims=True)   # ddof=0 par défaut → population
-    return (X - mu) / np.maximum(sigma, eps)
+    sigma = np.clip(sigma,a_min = eps,a_max=None)
+    return (X - mu) / sigma
