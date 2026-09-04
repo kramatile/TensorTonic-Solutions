7e4157c6404f7757449bf0f0777660dad6f0f0c2
Yassine Ben Fatah <94804124+kramatile@users.noreply.github.com>
2026-09-04 19:50:24 +0200

    Solve causal-masking

--- causal-masking/causal-masking.py
+++ causal-masking/causal-masking.py
@@ -7,10 +7,9 @@ def apply_causal_mask(scores: list, mask_value: float = -1e9) -> np.ndarray:
     q_len, k_len = scores.shape[-2:]
     scores = np.asarray(scores)
     n_dim = scores.ndim
-    mask = np.empty((q_len,k_len),bool)
-    for i in range(q_len):
-        for j in range(k_len):
-            mask[i][j] = j > i 
+    q_idx = np.arange(q_len)[:,None]
+    k_idx = np.arange(k_len)[None,:]
+    mask = k_idx > q_idx
 
     mask = np.where(mask,mask_value,scores)
     return mask
